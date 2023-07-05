8cc9ff7786874d96b73c37a2174677ead2e102b6
Xiao-Yang Zhi <mselab@126.com>
2023-07-05 12:11:37 +0800

    create class Proteome

--- pfam.py
+++ pfam.py
@@ -1,2 +1,13 @@
 
-import pyfasta
+import os
+from multiprocessing import Pool
+from pyfasta import Fasta
+
+
+class Proteome(Fasta):
+
+    def __init__(self, fasta_name):
+        super().__init__(fasta_name)
+
+    def do_hmmscan(self):
+        return
