3d198584089f5e7e955ec8eb9340c02979569117
Xiao-Yang Zhi <mselab@126.com>
2023-07-05 14:04:01 +0800

    初步结构化

--- modules/pfam.py
+++ modules/pfam.py
@@ -1,7 +1,24 @@
 
 import os
+from tempfile import TemporaryDirectory
+
 from multiprocessing import Pool
 from pyfasta import Fasta
+
+
+class Hit:
+
+    def __init__(self):
+        pass
+
+    def compare(self, other):
+        pass
+
+
+class Protein:
+
+    def __init__(self):
+        pass
 
 
 class Proteome(Fasta):
