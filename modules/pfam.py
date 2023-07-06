e64b3a84bac7b47764d74df01aa48a64b9ac6b82
Ling <1192224270@qq.com>
2023-07-06 18:19:27 +0800

    2023年7月6日测试gitee

--- modules/pfam.py
+++ modules/pfam.py
@@ -3,12 +3,16 @@ import os
 
 class Pfam:
 
-    def __init__(self, name, id, length):
+    def __init__(self, name, id, length, start, end, hitlength):
         self.name = name
         self.id = id
         self.length = length
+        self.start = start
+        self.end = end
+        self.hitlength = hitlength
 
     def relation(self, other):
+        # 用于计算两个pfam之间的关系
         return
 
 
@@ -25,12 +29,17 @@ class Hits(list):
 
     def _parse(self):
         for i in self.raw:
-            ilist = [_ for _ in i.split(' ') if _ is not '']
+            ilist = [_ for _ in i.split(' ') if _ != '']
             pfam_name = ilist[0]
             pfam_id = ilist[1]
-            pfam_length = ilist[3]
-            aPfam = Pfam(name=pfam_name, id=pfam_id, length=pfam_length)
+            pfam_length = ilist[2]
+            pfam_start = ilist[17]
+            pfam_end = ilist[18]
+            pfam_hit_length = int(pfam_end)-int(pfam_start)
+            aPfam = Pfam(name=pfam_name, id=pfam_id, length=pfam_length,
+                         start=pfam_start, end=pfam_end, hitlength=pfam_hit_length)
             self.append(aPfam)
 
     def ana_relations(self):
+        # 用于判断这个Hit里要保留哪个pfam
         pass
