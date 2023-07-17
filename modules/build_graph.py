088d9521580acf9df46406a27539b449884e55c3
Ling <1192224270@qq.com>
2023-07-17 17:24:36 +0800

    2023年7月17日修改make_pfam_graph()

--- modules/build_graph.py
+++ modules/build_graph.py
@@ -1,24 +1,36 @@
 from itertools import combinations
 from igraph import Graph
 
-def get_edges(self):  # 这个self是一个list，每个元素的一个pfam的对象
 
-	# 建立各个pfam_id对应的orf_id, pfam_id为key
+def get_edges(members):
+
 	pf_dic = {}
-	for i in range(len(self)):
-		if self[i].id not in pf_dic:
-			pf_dic[self[i].id] = [self[i].orf]
-		else:
-			pf_dic[self[i].id].append(self[i].orf)
+	orf_list = []
+
+	for member in members:
+		with open(member) as f:
+			contents = f.readlines()
+		content_list = [content.split('\t') for content in contents]
+
+		for i in range(len(content_list)):
+			orf = content_list[i][0]
+			orf_list.append(orf)
+			pf = list(set(content_list[i][1].rstrip().split(';')))
+			for j in pf:
+				if j != 'None':
+					if j not in pf_dic:
+						pf_dic[j] = [orf]
+					else:
+						pf_dic[j].append(orf)
 
 	edges_set = set()
 	for pfam in pf_dic.keys():
 		if len(pf_dic[pfam]) > 1:
-			edges_set = edges_set | set(combinations(sorted(self[pfam]), 2))
+			edges_set = edges_set | set(combinations(sorted(pf_dic[pfam]), 2))
 		# 对pfam对应的orf列表组合，然后取并集，得到所有边的情况
 		# sorted后可以消除(a,b)和(b,a)不一致的情况
 	edges = list(edges_set)  # 边
-	vertices = pf_dic.keys()  # 点
+	vertices = list(set(orf_list))  # 点
 	return vertices, edges
 
 
