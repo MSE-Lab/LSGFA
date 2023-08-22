2dfa3fbbb591c152ea73f238795897613ecb4343
Ling <1192224270@qq.com>
2023-08-22 13:51:30 +0800

    2023年8月22日增加连接条件，修改社区发现方法

--- modules/build_graph.py
+++ modules/build_graph.py
@@ -3,37 +3,50 @@ from igraph import Graph
 import leidenalg as la
 
 
+def co_index(lista, listb):
+	# 用于判断两个ORF之间Pfam的关联度
+	set_len = len(set(lista) & set(listb))
+	if set_len == 0:
+		co_number = 0
+	else:
+		co_number = (set_len/len(lista) + set_len/len(listb))*0.5
+	return co_number
+
+
 def get_edges(members):
 	# 用于获取点和边
+	# members是.Pfam的文件
 
-	pf_dic = {}
+	orf_pfam_dic = {}
 	orf_list = []
+	edges_list = []
 
 	for member in members:
 		with open(member) as f:
 			contents = f.readlines()
 		content_list = [content.split('\t') for content in contents]
 
+		# 建立orf对应的pfam的字典
 		for i in range(len(content_list)):
 			orf = content_list[i][0]
 			orf_list.append(orf)
 			pf = list(set(content_list[i][1].rstrip().split(';')))
-			for j in pf:
-				if j != 'None':
-					if j not in pf_dic:
-						pf_dic[j] = [orf]
-					else:
-						pf_dic[j].append(orf)
+			if pf != ['None']:
+				orf_pfam_dic[orf] = pf
 
-	edges_set = set()
-	for pfam in pf_dic.keys():
-		if len(pf_dic[pfam]) > 1:
-			edges_set = edges_set | set(combinations(sorted(pf_dic[pfam]), 2))
-		# 对pfam对应的orf列表组合，然后取并集，得到所有边的情况
-		# sorted后可以消除(a,b)和(b,a)不一致的情况
-	edges = list(edges_set)  # 边
+	# 判断两个orf之间是否可以连线
+	key_list = sorted(list(orf_pfam_dic.keys()))
+	n = len(key_list)
+	for i in range(n):
+		lista = orf_pfam_dic[key_list[i]]
+		for j in range(i+1, n):
+			listb = orf_pfam_dic[key_list[j]]
+			if co_index(lista, listb) >= 0.6:
+				edges_list.append((key_list[i], key_list[j]))
+
+	# 边为：edges_list
 	vertices = list(set(orf_list))  # 点
-	return vertices, edges
+	return vertices, edges_list
 
 
 def build_graph(orf_id, edges):
@@ -47,10 +60,11 @@ def build_graph(orf_id, edges):
 
 def graph_split(self):
 	# 对网络进行社区发现
-
-	partitions = la.find_partition(self, la.ModularityVertexPartition)
-	partitions_names = []
-	for cs in partitions:
-		partitions_names.append([self.vs[i]['name'] for i in cs])
-	print(partitions_names)
-	return partitions_names
+	subgraphs = self.components().subgraphs()
+	communities = []
+	for subgraph in subgraphs:
+		partition = la.find_partition(subgraph, la.ModularityVertexPartition)
+		for community in partition:
+			community_names = [subgraph.vs[node]["name"] for node in community]
+			communities.append(community_names)
+	return communities
